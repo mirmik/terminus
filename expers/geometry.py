d21317a00b42b5c8017b00dfacb8a0ce169c8bde
mirmik <mirmikns@yandex.ru>
2023-05-28 22:16:32 +0300

    'motor'

--- expers/geometry.py
+++ expers/geometry.py
@@ -3,8 +3,10 @@
 from terminus.zencad_adapter import draw_line2, draw_point2, draw_line2_positive
 from terminus.ga201.line import Line
 from terminus.ga201.point import Point
+from terminus.ga201.motor import Motor
 from terminus.ga201.join import join_point_point, oriented_distance
 from terminus.ga201.convex_hull import ConvexBody
+import math
 
 import zencad
 zencad.disable_lazy()
@@ -40,5 +42,21 @@ proj = convex.point_projection(p)
 draw_point2(p)
 draw_point2(proj)
 print("proj", proj)
+
+print("***")
+print("***")
+q1 = Motor.rotation(math.pi/2)
+q2 = Motor.translation(20,0)
+q3 = Motor.rotation(-math.pi/2)
+q = q1*q2*q3
+print("q1", q1)
+print("q2", q2)
+print("q", q)
+
+p = Point(1,0)
+qp = q.transform(p)
+
+print("***")
+print(qp)
 
 zencad.show()
--- terminus/ga201/motor.py
+++ terminus/ga201/motor.py
@@ -1,6 +1,7 @@
 
 import math
 
+from terminus.ga201.point import Point
 
 class Motor:
     def __init__(self, x, y, z, w):
@@ -11,23 +12,46 @@ class Motor:
 
     @staticmethod
     def rotation(rads):
-        z = math.sin(rads)
-        w = math.cos(rads)
+        z = math.sin(rads/2)
+        w = math.cos(rads/2)
         return Motor(0, 0, z, w)
 
     @staticmethod
-    def translation(x, y)
-        x = x
-        y = y
+    def translation(x, y):
+        x = x/2
+        y = y/2
         z = 0
         w = 1
+        return Motor(x, y, z, w)
 
     def __mul__(self, other):
         q = self
         p = other
         return Motor(
-            q.w*p.x + q.x*p.w + q.z*p.y - q.y*p.z,
-            q.w*p.y + q.y*p.w + q.x*p.z - q.z*p.x,
-            q.w*q.z + q.z*p.w,
-            q.w * p.w - q.z * p.z
+            q.w*p.x + q.x*p.w - q.z*p.y + q.y*p.z,
+            q.w*p.y + q.y*p.w - q.x*p.z + q.z*p.x,
+            q.w*p.z + q.z*p.w,
+            q.w*p.w - q.z*p.z
         )
+
+    def transform_point(self, p):
+        q = self
+        a = q.w
+        a12 = q.z
+        a31 = q.y
+        a23 = q.x
+        return Point(
+            (a**2 - a12**2)*p.x - 2*a*a12*p.y + (2*a*a23 - 2*a12*a31)*p.z,
+            (a**2 - a12**2)*p.y + 2*a*a12*p.x + (2*a*a31 + 2*a12*a23)*p.z,
+            (a**2 + a12**2)*p.z
+        )
+
+    def transform(self, o):
+        if isinstance(o, Point):
+            return self.transform_point(o)
+
+    def __repr__(self):
+        return "Motor(%s, %s, %s, %s)" % (self.x, self.y, self.z, self.w)
+    
+    def __str__(self):
+        return repr(self)
