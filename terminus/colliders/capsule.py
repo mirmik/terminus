6f08c8bd59cee6a68071532be6dbbb370cf02c4e
netricks <mirmikns@yandex.ru>
2025-10-30 01:37:42 +0300

    Refactor collider classes: implement SphereCollider and UnionCollider, and update closest point calculations

--- terminus/colliders/capsule.py
+++ terminus/colliders/capsule.py
@@ -2,44 +2,7 @@
 from terminus.clossest import closest_points_between_segments, closest_points_between_capsules, closest_points_between_capsule_and_sphere
 import numpy
 from terminus.colliders.collider import Collider
-
-class SphereCollider(Collider):
-    def __init__(self, center: numpy.ndarray, radius: float):
-        self.center = center
-        self.radius = radius
-
-    def transform_by(self, transform: 'Transform3'):
-        """Return a new SphereCollider transformed by the given Transform3."""
-        new_center = transform.transform_point(self.center)
-        return SphereCollider(new_center, self.radius)
-
-    def closest_to_sphere(self, other: "SphereCollider"):
-        """Return the closest points and distance between this sphere and another sphere."""
-        center_dist = numpy.linalg.norm(other.center - self.center)
-        dist = center_dist - (self.radius + other.radius)
-        if center_dist > 1e-8:
-            dir_vec = (other.center - self.center) / center_dist
-        else:
-            dir_vec = numpy.array([1.0, 0.0, 0.0])  # Arbitrary direction if centers coincide
-        p_near = self.center + dir_vec * self.radius
-        q_near = other.center - dir_vec * other.radius
-        return p_near, q_near, dist
-
-    def closest_to_capsule(self, other: "CapsuleCollider"):
-        """Return the closest points and distance between this sphere and a capsule."""
-        p_near, q_near, dist = closest_points_between_capsule_and_sphere(
-            other.a, other.b, other.radius,
-            self.center, self.radius)
-        return q_near, p_near, dist
-
-    def closest_to_collider(self, other: "Collider"):
-        """Return the closest points and distance between this collider and another collider."""
-        if isinstance(other, SphereCollider):
-            return self.closest_to_sphere(other)
-        elif isinstance(other, CapsuleCollider):
-            return self.closest_to_capsule(other)
-        else:
-            raise NotImplementedError("closest_to_collider not implemented for this collider type.")
+from terminus.colliders.sphere import SphereCollider
 
 class CapsuleCollider(Collider):
     def __init__(self, a: numpy.ndarray, b: numpy.ndarray, radius: float):
@@ -67,11 +30,22 @@ class CapsuleCollider(Collider):
             other.center, other.radius)
         return p_near, q_near, dist
 
+    def closest_to_union_collider(self, other: "UnionCollider"):
+        """Return the closest points and distance between this capsule and a union collider."""
+        a,b,c = other.closest_to_collider(self)
+        return b,a,c
+
     def closest_to_collider(self, other: "Collider"):
         """Return the closest points and distance between this collider and another collider."""
+
+        from .capsule import SphereCollider
+        from .union_collider import UnionCollider
+
         if isinstance(other, CapsuleCollider):
             return self.closest_to_capsule(other)
         elif isinstance(other, SphereCollider):
             return other.closest_to_sphere(self)
+        elif isinstance(other, UnionCollider):
+            return self.closest_to_union_collider(other)
         else:
-            raise NotImplementedError("closest_to_collider not implemented for this collider type.")
+            raise NotImplementedError(f"closest_to_collider not implemented for {type(other)}")
---
+++ utest/collider_test.py
@@ -0,0 +1,114 @@
+from terminus.colliders.capsule import CapsuleCollider
+from terminus.colliders.sphere import SphereCollider
+from terminus.colliders.union_collider import UnionCollider
+import unittest
+import numpy
+
+class TestCollider(unittest.TestCase):
+    def test_closest_to_capsule(self):
+        capsule1 = CapsuleCollider(
+            a = numpy.array([0.0, 0.0, 0.0]),
+            b = numpy.array([0.0, 0.0, 1.0]),
+            radius = 0.25
+        )
+        capsule2 = CapsuleCollider(
+            a = numpy.array([1.0, 0.0, 0.0]),
+            b = numpy.array([1.0, 0.0, 0.0]),
+            radius = 0.25
+        )
+
+        p_near, q_near, dist = capsule1.closest_to_capsule(capsule2)
+
+        expected_dist = 0.5  # Capsules are touching
+        self.assertAlmostEqual(dist, expected_dist)
+
+        expected_p_near = numpy.array([0.25, 0.0, 0.0])
+        expected_q_near = numpy.array([0.75, 0.0, 0.0])
+
+        numpy.testing.assert_array_almost_equal(p_near, expected_p_near)
+        numpy.testing.assert_array_almost_equal(q_near, expected_q_near)
+
+    def test_closest_sphere_to_capsule(self):
+        capsule = CapsuleCollider(
+            a = numpy.array([0.0, 0.0, 0.0]),
+            b = numpy.array([0.0, 0.0, 2.0]),
+            radius = 0.5
+        )
+        sphere_center = numpy.array([1.0, 0.0, 1.0])
+        sphere_radius = 0.5
+
+        sphere = SphereCollider(
+            center = sphere_center,
+            radius = sphere_radius
+        )
+
+        p_near, q_near, dist = sphere.closest_to_capsule(capsule)
+
+        expected_dist = 0.0  # They are touching
+        self.assertAlmostEqual(dist, expected_dist)
+
+        expected_p_near = numpy.array([0.5, 0.0, 1.0])
+        expected_q_near = numpy.array([0.5, 0.0, 1.0])
+
+        numpy.testing.assert_array_almost_equal(p_near, expected_p_near)
+        numpy.testing.assert_array_almost_equal(q_near, expected_q_near)
+
+    def test_closest_union_collider(self):
+        sphere1 = SphereCollider(
+            center = numpy.array([0.0, 0.0, 0.0]),
+            radius = 0.5
+        )
+        sphere2 = SphereCollider(
+            center = numpy.array([3.0, 0.0, 0.0]),
+            radius = 0.5
+        )
+        union_collider = UnionCollider([sphere1, sphere2])
+
+        test_sphere = SphereCollider(
+            center = numpy.array([0.0, 1.5, 0.0]),
+            radius = 0.5
+        )
+
+        p_near, q_near, dist = union_collider.closest_to_collider(test_sphere)
+
+        expected_dist = 0.5  # Closest to sphere1
+        self.assertAlmostEqual(dist, expected_dist)
+
+        expected_p_near = numpy.array([0.0, 0.5, 0.0])
+        expected_q_near = numpy.array([0.0, 1.0, 0.0])
+
+        numpy.testing.assert_array_almost_equal(p_near, expected_p_near)
+        numpy.testing.assert_array_almost_equal(q_near, expected_q_near)
+
+    def test_two_union_colliders(self):
+        sphere1 = SphereCollider(
+            center = numpy.array([0.0, 0.0, 0.0]),
+            radius = 0.5
+        )
+        sphere2 = SphereCollider(
+            center = numpy.array([3.0, 0.0, 0.0]),
+            radius = 0.5
+        )
+        union_collider1 = UnionCollider([sphere1, sphere2])
+
+        sphere3 = SphereCollider(
+            center = numpy.array([1.25, 0.0, 0.0]),
+            radius = 0.5
+        )
+        sphere4 = SphereCollider(
+            center = numpy.array([5.0, 0.0, 0.0]),
+            radius = 0.5
+        )
+        union_collider2 = UnionCollider([sphere3, sphere4])
+
+        p_near, q_near, dist = union_collider1.closest_to_collider(union_collider2)
+
+        expected_dist = 0.25  # Closest between sphere2 and sphere3
+        self.assertAlmostEqual(dist, expected_dist)
+
+        expected_p_near = numpy.array([0.5, 0.0, 0.0])
+        expected_q_near = numpy.array([0.75, 0.0, 0.0])
+
+        numpy.testing.assert_array_almost_equal(p_near, expected_p_near)
+        numpy.testing.assert_array_almost_equal(q_near, expected_q_near)
+        
